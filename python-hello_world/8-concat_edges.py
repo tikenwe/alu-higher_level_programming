4ffb8b2793326dae5203318e7e525b6580f8256a
tikenwe <t.ikenwe@alustudent.com>
2026-06-26 02:22:08 -0700

    Add 8-concat_edges.py script

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(str[44:71] + str[114:119] + str[:6])
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+print(str[39:66] + str[106:112] + str[:6])

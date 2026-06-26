e6b40e28b376a9c22e075f7d4af5c5a45f266196
tikenwe <t.ikenwe@alustudent.com>
2026-06-26 04:07:54 -0700

    Add 2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    count = len(argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(count))
+        for i in range(1, count + 1):
+            print("{}: {}".format(i, argv[i]))
